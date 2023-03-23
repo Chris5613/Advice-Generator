cad47d4e58d887e12e494572b9de4e2e6a715bea
Christian Wu <christianwu56@gmail.com>
2023-03-22 18:17:03 -0700

    added svg divider

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,6 +1,6 @@
 
 const Box = () => {
-  return (
+return (
     <div className="main-container">
         <div className="title-container">
             <h3 className="title">A d v i c e  <span id="num"># 1</span></h3>
@@ -12,8 +12,18 @@ const Box = () => {
                 up and taking action."
             </p>
         </div>
+        <div className="divider">
+            <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fill-rule="evenodd">
+                    <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/>
+                    <g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/>
+                        <rect x="14" width="6" height="16" rx="3"/>
+                    </g>
+                </g>
+            </svg>
+        </div>
     </div>
-  )
+)
 }
 
 export default Box
