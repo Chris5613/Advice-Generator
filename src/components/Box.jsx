c6f8753c866f101b80ce746f803f7205763f1293
Christian Wu <christianwu56@gmail.com>
2023-03-22 18:06:29 -0700

    created main box container with title and hardcode text

---
+++ src/components/Box.jsx
@@ -0,0 +1,19 @@
+
+const Box = () => {
+  return (
+    <div className="main-container">
+        <div className="title-container">
+            <h3 className="title">A d v i c e  <span id="num"># 1</span></h3>
+        </div>
+        <div>
+            <p className="main-text">
+                "It is easy to situp and take <br />
+                notice, what's difficult is getting <br />
+                up and taking action."
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Box
